8da59db98c115372d500359ddd0fb3fb804f9721
radzek15 <radzek15@wp.pl>
2023-10-31 15:14:37 -0400

    docker, nginx, postgres

--- CodeGen/settings/base.py
+++ CodeGen/settings/base.py
@@ -52,14 +52,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CodeGen.wsgi.application'
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = {"default": env.db("DATABASE_URL")}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
